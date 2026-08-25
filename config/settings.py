133ae0603971647e4fa0ab9db63a30bb0af532fe
vlad <vladlen13372288800@gmail.com>
2026-08-25 17:04:06 +0300

    Connect PostgreSQL database

--- config/settings.py
+++ config/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'crm_db',
+        'USER': 'crm_user',
+        'PASSWORD': '25082001',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
